c69109745a42198a99795d460d798a56842c9a8a
Marija <marijazdravic@gmail.com>
2025-06-10 22:53:08 +0200

    Replace load command completion to return a result type rather than an optional error

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -30,17 +30,37 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         
         var receivedError: Error?
-        sut.load() { error in
-            receivedError =  error
+        sut.load() { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure, got /(result) instead")
+            }
             exp.fulfill()
         }
-        
         store.completeRetrieval(with: retrievalError)
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
+//    func test_load_deliversNoImagesOnEmptyCache() {
+//        let (sut, store) = makeSUT()
+//        let exp = expectation(description: "Wait for load completion")
+//        
+//        var receivedImages: [FeedImage]?
+//        sut.load() { result in
+//            
+//        }
+//        
+//        store.completeRetrieval(with: retrievalError)
+//        wait(for: [exp], timeout: 1.0)
+//        
+//        XCTAssertEqual(receivedError as NSError?, retrievalError)
+//        
+//    }
+//    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
