cb4789b06f0297f7a29edd9540e7998342cefb23
Marija <marijazdravic@gmail.com>
2025-06-16 16:58:32 +0200

    Load persistent container stores upon CoreDataFeedStore initialization

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -9,8 +9,11 @@ import Foundation
 import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
+    private let container: NSPersistentContainer
     
-    public init() {}
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
@@ -28,6 +31,7 @@ public final class CoreDataFeedStore: FeedStore {
         
     }
 }
+
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
@@ -39,4 +43,33 @@ private class ManagedFeedImage: NSManagedObject {
     @NSManaged var location: String?
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
+}
+
+private extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer{
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
 }
