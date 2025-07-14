3a33c6147a1000b1f57a24ee06f43ae253684911
Marija <marijazdravic@gmail.com>
2025-07-14 15:31:01 +0200

    Group Controllers and Views in Feed UI folder

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/FeedImageCell.swift
@@ -0,0 +1,33 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Marija Zdravic on 03.07.2025..
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+    
+    private(set) public lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button
+            .addTarget(
+                self,
+                action: #selector(retryButtonTapped),
+                for: .touchUpInside
+            )
+        return button
+    }()
+    
+    var onRetry: (() -> Void)?
+    
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
+}
