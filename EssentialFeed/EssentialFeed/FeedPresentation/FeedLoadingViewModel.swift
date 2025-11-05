72047f057c0849ab81cee9830a7b89180d612df8
Marija <marijazdravic@gmail.com>
2025-11-05 19:58:53 +0100

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Marija Zdravic on 05.11.2025..
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
+
