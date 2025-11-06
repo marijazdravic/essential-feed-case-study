17ac3e6acb598a48028f56009ac3fb18be353756
Marija <marijazdravic@gmail.com>
2025-11-06 18:52:07 +0100

    Move FeedImageViewModel to separate file

--- EssentialFeed/EssentialFeed/FeedPresentation/FeedImagePresenter.swift
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedImagePresenter.swift
@@ -1,22 +1,10 @@
 //
-//  FeedImagePresentr.swift
+//  FeedImagePresenter.swift
 //  EssentialFeed
 //
 //  Created by Marija Zdravic on 06.11.2025..
 //
 import Foundation
-
-public struct FeedImageViewModel<Image> {
-    public let description: String?
-    public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
-    
-    public var hasLocation: Bool {
-        return location != nil
-    }
-}
 
 public protocol FeedImageView {
     associatedtype Image
