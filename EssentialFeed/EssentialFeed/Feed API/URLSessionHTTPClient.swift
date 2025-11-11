9d791e5aad24ce3911349320dabde6630f37e0ed
Marija <marijazdravic@gmail.com>
2025-11-11 16:51:04 +0100

    Rename test case class to reflect the use case it refers to

--- EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-public class URLSessionHTTPClient: HTTPClient {
-
+public final class URLSessionHTTPClient: HTTPClient {
+    
     private let session: URLSession
     
     public init(session: URLSession) {
@@ -25,9 +25,9 @@ public class URLSessionHTTPClient: HTTPClient {
         }
     }
     
-    public func get(from url: URL, completion: @escaping(HTTPClient.Result) -> Void)  ->  HTTPClientTask {
+    public func get(from url: URL, completion: @escaping(HTTPClient.Result) -> Void) ->  HTTPClientTask {
         let task = session.dataTask(with: url) { data, response, error in
-            completion( Result {
+            completion(Result {
                 if let error = error {
                     throw error
                 } else if let data = data, let response = response as? HTTPURLResponse {
@@ -36,7 +36,6 @@ public class URLSessionHTTPClient: HTTPClient {
                     throw UnexpectedValuesRepresentation()
                 }
             })
-            
         }
         
         task.resume()
