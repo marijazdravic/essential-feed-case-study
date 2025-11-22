c90dc6e375b811f914862308bfef9b34b4956bfa
Marija <marijazdravic@gmail.com>
2025-11-22 20:26:41 +0100

    FeedImageDataLoaderWithFallbackComposite.init does not load image data

---
+++ EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,58 @@
+//
+//  FeedImageLoaderWithFallbackCompositeTests.swift
+//  EssentialAppTests
+//
+//  Created by Marija Zdravic on 21.11.2025..
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private class Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) ->
+    FeedImageDataLoaderTask {
+        return Task()
+    }
+}
+
+class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = ImageLoaderSpy()
+        let fallbackLoader = ImageLoaderSpy()
+        _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private class ImageLoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() {}
+        }
+        
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            
+            return Task()
+        }
+    }
+}
