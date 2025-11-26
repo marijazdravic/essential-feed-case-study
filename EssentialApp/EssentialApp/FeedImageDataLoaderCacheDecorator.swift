4f1a65ce3eec1a0d0024a13e15a7080c48a62954
Marija <marijazdravic@gmail.com>
2025-11-26 22:04:34 +0100

    Move FeedImageDataLoaderCacheDecorator to production

---
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,29 @@
+//
+//  FeedImageDataLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Marija Zdravic on 26.11.2025..
+//
+
+import Foundation
+import EssentialFeed
+
+public class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    let decoratee: FeedImageDataLoader
+    let cache: FeedImageDataCache
+    
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [ weak self ] result in
+            completion(result.map { data in
+                self?.cache.save(data, for: url) { _ in }
+                return data
+            })
+        }
+    }
+}
+
