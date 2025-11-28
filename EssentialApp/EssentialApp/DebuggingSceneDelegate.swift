f90b3a77705bb6664df1efdf4615a0072c460ce8
Marija <marijazdravic@gmail.com>
2025-11-28 18:56:04 +0100

    Move DEBUG code paths from the main SceneDelegate to a new DebuggingSceneDelegate subclass to separate debug- and test-specific code from production code

---
+++ EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  DebuggingSceneDelegate.swift
+//  EssentialApp
+//
+//  Created by Marija Zdravic on 28.11.2025..
+//
+#if DEBUG
+
+import Foundation
+import UIKit
+import EssentialFeed
+
+
+class DebuggingSceneDelegate: SceneDelegate {
+    
+    override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if CommandLine.arguments.contains("-reset") {
+            try? FileManager.default.removeItem(at: localStoreURL)
+        }
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
+    }
+    
+    override func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            return AlwaysFailingHTTPClient()
+            
+        default:
+            return super.makeRemoteClient()
+        }
+    }
+}
+    
+
+private class AlwaysFailingHTTPClient: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        completion(.failure(NSError(domain: "offline", code: 0)))
+        return Task()
+    }
+}
+#endif
