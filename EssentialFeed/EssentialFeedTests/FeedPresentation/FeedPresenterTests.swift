e26e58f0bc03cf3ae42e61e48e32292ae01a81ef
Marija <marijazdravic@gmail.com>
2026-01-21 12:52:48 +0100

    Fix FeedPresentationTests

--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -82,19 +82,20 @@ class FeedPresenterTests: XCTestCase {
         return value
     }
     
-    private class ViewSpy: FeedView, ResourceErrorView, ResourceLoadingView {
+    private class ViewSpy: FeedView, ResourceLoadingView, ResourceErrorView {
         enum Message: Hashable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
             case display(feed: [FeedImage])
         }
-        private(set)var messages = Set<Message>()
         
-        func display(_ viewModel: FeedErrorViewModel) {
+        private(set) var messages = Set<Message>()
+        
+        func display(_ viewModel: ResourceErrorViewModel) {
             messages.insert(.display(errorMessage: viewModel.message))
         }
         
-        func display(_ viewModel: FeedLoadingViewModel) {
+        func display(_ viewModel: ResourceLoadingViewModel) {
             messages.insert(.display(isLoading: viewModel.isLoading))
         }
         
