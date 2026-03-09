126520c17d8b02dfa375530a479124f1fc33b828
Marija <marijazdravic@gmail.com>
2026-03-09 15:14:07 +0100

    Make FeedCache sync

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -110,11 +110,11 @@ private extension FeedImageDataCache {
 
 private extension FeedCache {
     func saveIgnoringResult(_ feed: [FeedImage]) {
-        save(feed) { _ in }
+        try? save(feed)
     }
     
     func saveIgnoringResult(_ page: Paginated<FeedImage>) {
-        save(page.items) { _ in }
+        try? save(page.items)
     }
 }
 
