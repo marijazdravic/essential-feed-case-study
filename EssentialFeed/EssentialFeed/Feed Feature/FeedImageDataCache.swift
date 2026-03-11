0f05531c3b921488e4a5724ad4ed8e1c60f76d68
Marija <marijazdravic@gmail.com>
2026-03-11 23:00:39 +0100

    Make FeedImageDataCache sync

--- EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataCache.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataCache.swift
@@ -8,8 +8,6 @@
 import Foundation
 
 public protocol FeedImageDataCache {
-    typealias SaveResult = Result<Void, Swift.Error>
-    
-    func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void)
+    func save(_ data: Data, for url: URL) throws
 }
 
