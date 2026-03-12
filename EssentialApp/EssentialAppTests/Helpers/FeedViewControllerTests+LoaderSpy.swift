1220af0f93f53f70d1e85433b9c51e9bd299aad1
Marija <marijazdravic@gmail.com>
2026-03-12 13:31:11 +0100

    Make FeedImageDataLoader sync

--- EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -11,12 +11,15 @@ import EssentialFeed
 import Combine
 import EssentialApp
 
-class LoaderSpy: FeedImageDataLoader {
+class LoaderSpy {
     
     //MARK: Feed Loader
     
     private var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
-    private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+    
+    var loadFeedCallCount: Int {
+        feedRequests.count
+    }
     
     func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
         let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
@@ -38,9 +41,7 @@ class LoaderSpy: FeedImageDataLoader {
     
     // MARK: - LoadMoreFeedLoader
     
-    var loadFeedCallCount: Int {
-        feedRequests.count
-    }
+    private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
     
     var loadMoreCallCount: Int {
         loadMoreRequests.count
@@ -67,36 +68,28 @@ class LoaderSpy: FeedImageDataLoader {
     
     //MARK: FeedImageDataLoader
     
-    private struct TaskSpy: FeedImageDataLoaderTask {
-        let cancelCallback: () -> Void
-        
-        func cancel() {
-            cancelCallback()
-        }
-    }
-    
-    private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+    private var imageRequests = [(url: URL, publisher: PassthroughSubject<Data, Error>)]()
     
     var loadedImageURLs: [URL] {
         imageRequests.map { $0.url }
     }
     
-    var canceledImageURLs = [URL]()
+    var cancelledImageURLs = [URL]()
     
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        imageRequests.append((url, completion))
-        return TaskSpy {
-            [weak self] in self?.canceledImageURLs.append(url)
-        }
+    func loadImageDataPublisher(from url: URL) -> AnyPublisher<Data, Error> {
+        let publisher = PassthroughSubject<Data, Error>()
+        imageRequests.append((url, publisher))
+        return publisher.handleEvents(receiveCancel: { [weak self] in
+            self?.cancelledImageURLs.append(url)
+        }).eraseToAnyPublisher()
     }
     
     func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-        imageRequests[index].completion(.success(imageData))
+        imageRequests[index].publisher.send(imageData)
+        imageRequests[index].publisher.send(completion: .finished)
     }
     
     func completeImageLoadingWithError(at index: Int = 0) {
-        let error = NSError(domain: "an error", code: 0)
-        imageRequests[index].completion(.failure(error))
+        imageRequests[index].publisher.send(completion: .failure(anyNSError()))
     }
 }
-
