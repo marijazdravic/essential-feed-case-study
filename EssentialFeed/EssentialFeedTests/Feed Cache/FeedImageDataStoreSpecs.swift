e52d85d2e2ac588b53ec35215937ee6d4784d17b
Marija <marijazdravic@gmail.com>
2026-03-15 18:48:59 +0100

    Extract reusable FeedImageDataStoreSpecs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedImageDataStoreSpecs.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStoreSpecs.swift
+//  EssentialAppTests
+//
+//  Created by Marija Zdravic on 15.03.2026..
+//
+
+import Foundation
+
+protocol FeedImageDataStoreSpecs {
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws
+    func test_retrieveImageData_deliversLastInsertedValue() throws
+}
